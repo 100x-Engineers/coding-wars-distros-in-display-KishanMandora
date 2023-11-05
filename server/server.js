30c7ef4ed40a3162922865111845f5235d72bee0
DecodersLord <PRIYANK.SEVAK1996@GMAIL.COM>
2023-11-05 01:59:49 -0500

    pushing back-end

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ app.use(express.json());
 const port = process.env.PORT || 3000;
 
 app.get('/', (req,res) => {
-    res.send('<h1>Hello World!!</h1>')
+    res.send('<h1>Hello World!!!!</h1>')
 })
 
 app.listen(port, () => {
